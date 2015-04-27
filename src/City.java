fdede24c74df20763dffbb9524bc0a3f1bb17ac3
Nauman Shahzad <nauman.shahzad@stonybrook.edu>
2015-04-27 14:22:18 -0400

    Initial commit

---
+++ src/City.java
@@ -0,0 +1,69 @@
+import latlng.LatLng;
+
+/**
+ * Nauman Shahzad
+ * 109813732
+ * Homework #7 - CSE 214
+ * Thursday: R04
+ * Gustavo Poscidonio
+ * Mahsa Torkaman
+ * @author Nauman
+ */
+
+public class City
+{
+    private String city;
+    private LatLng location;
+    private int indexPos;
+    private static int cityCount;
+
+    public City()
+    {
+        if(cityCount == 0)
+            cityCount = 0;
+        indexPos = cityCount;
+        cityCount++;
+    }
+
+    public City(String city)
+    {
+        if(cityCount == 0)
+            cityCount = 0;
+        indexPos = cityCount;
+        cityCount++;
+
+        this.city = city;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public LatLng getLocation() {
+        return location;
+    }
+
+    public void setLocation(LatLng location) {
+        this.location = location;
+    }
+
+    public int getIndexPos() {
+        return indexPos;
+    }
+
+    public void setIndexPos(int indexPos) {
+        this.indexPos = indexPos;
+    }
+
+    public static int getCityCount() {
+        return cityCount;
+    }
+
+    public static void setCityCount(int cityCount) {
+        City.cityCount = cityCount;
+    }
+}
---
+++ src/SigmaAir.java
@@ -0,0 +1,106 @@
+/**
+ * Nauman Shahzad
+ * 109813732
+ * Homework #7 - CSE 214
+ * Thursday: R04
+ * Gustavo Poscidonio
+ * Mahsa Torkaman
+ * @author Nauman
+ */
+
+import com.google.code.geocoder.Geocoder;
+import com.google.code.geocoder.GeocoderRequestBuilder;
+import com.google.code.geocoder.model.GeocodeResponse;
+import com.google.code.geocoder.model.GeocoderRequest;
+import latlng.LatLng;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class SigmaAir
+{
+    private ArrayList<City> cities;
+    public static final int MAX_CITIES = 100;
+    private double[][] connections;
+
+    public SigmaAir()
+    {
+        cities = new ArrayList<City>();
+        connections = new double[MAX_CITIES][MAX_CITIES];
+    }
+
+    public void addCity(String city)
+    {
+        City toAdd = new City(city);
+        cities.add(toAdd);
+    }
+
+    public void addConnection(String cityFrom, String cityTo)
+    {
+        try{
+            Geocoder geocoder = new Geocoder();
+            GeocoderRequest geocoderRequest;
+            GeocodeResponse geocodeResponse;
+
+            double latFrom, latTo, lngFrom, lngTo;
+
+            geocoderRequest = new GeocoderRequestBuilder().setAddress(cityFrom).getGeocoderRequest();
+            geocodeResponse = geocoder.geocode(geocoderRequest);
+            latFrom = geocodeResponse.getResults().get(0).getGeometry().getLocation().getLat().doubleValue();
+            lngFrom = geocodeResponse.getResults().get(0).getGeometry().getLocation().getLng().doubleValue();
+
+            geocoderRequest = new GeocoderRequestBuilder().setAddress(cityTo).getGeocoderRequest();
+            geocodeResponse = geocoder.geocode(geocoderRequest);
+            latTo = geocodeResponse.getResults().get(0).getGeometry().getLocation().getLat().doubleValue();
+            lngTo = geocodeResponse.getResults().get(0).getGeometry().getLocation().getLng().doubleValue();
+
+            LatLng src = new LatLng(latFrom, lngFrom);
+            LatLng dest = new LatLng(latTo, lngTo);
+
+            City one = new City(cityFrom);
+            City two = new City(cityTo);
+            one.setLocation(src);
+            two.setLocation(dest);
+
+            cities.add(one);
+            cities.add(two);
+
+            double distance = LatLng.calculateDistance(src, dest);
+
+            connections[one.getIndexPos()][two.getIndexPos()] = distance;
+
+            connections[one.getIndexPos()][one.getIndexPos()] = 0;
+            connections[two.getIndexPos()][two.getIndexPos()]= 0;
+
+        }catch(IOException ex) {}
+    }
+
+    private City[] getCities(String cityFrom, String cityTo)
+    {
+        City[] citiesToReturn = new City[2];
+        for(int i = 0; i < cities.size(); i++)
+        {
+            if(cities.get(i).getCity().equalsIgnoreCase(cityFrom))
+                citiesToReturn[0] = cities.get(i);
+
+            if(cities.get(i).getCity().equalsIgnoreCase(cityTo))
+                citiesToReturn[1] = cities.get(i);
+        }
+
+        return citiesToReturn;
+    }
+
+    public void removeConnection(String cityFrom, String cityTo)
+    {
+        City[] connection = getCities(cityFrom, cityTo);
+
+        connections[connection[0].getIndexPos()][connection[1].getIndexPos()] = Double.POSITIVE_INFINITY;
+
+    }
+
+    public String shortestPath(String cityFrom, String cityTo)
+    {
+        City[] connection = getCities(cityFrom, cityTo);
+
+    }
+}
