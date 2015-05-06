6573f21b8b6e0a91393b2931f209859ad576be7c
Nauman Shahzad <nauman.shahzad@stonybrook.edu>
2015-05-06 13:45:55 -0400

    Javadoc for City class

--- src/City.java
+++ src/City.java
@@ -21,6 +21,10 @@ public class City
     private int indexPos;
     private static int cityCount;
 
+    /**
+     * No argument constructor for City class.
+     * <dt><b>Postconditions:</b></dd> A new City object has been made.
+     */
     public City()
     {
         if(cityCount == 0)
@@ -29,6 +33,11 @@ public class City
         cityCount++;
     }
 
+    /**
+     * Constructor that takes in title of City.
+     * @param city Name of the city.
+     * <dt><b>Postconditions:</b></dd> A new City object has been made with a specified city name.
+     */
     public City(String city)
     {
         if(cityCount == 0)
@@ -39,34 +48,79 @@ public class City
         this.city = city;
     }
 
+    /**
+     * Getter method for city name.
+     * @return Returns city name.
+     * <dt><b>Preconditions:</b></dd> City object must already be instantiated.
+     */
     public String getCity() {
         return city;
     }
 
+    /**
+     * Setter method for the city name.
+     * @param city New name to set city to.
+     * <dt><b>Preconditions:</b></dd> City object must already be instantiated.
+     * <dt><b>Postconditions:</b></dd> City name has been changed.
+     */
     public void setCity(String city) {
         this.city = city;
     }
 
+    /**
+     * Getter method for location of this city.
+     * @return Returns location of city.
+     * <dt><b>Preconditions:</b></dd> City object must already be instantiated.
+     */
     public LatLng getLocation() {
         return location;
     }
 
+    /**
+     * Setter method for the location.
+     * @param location New location for this city.
+     * <dt><b>Preconditions:</b></dd> City object must already be instantiated.
+     * <dt><b>Postconditions:</b></dd> Location of this city has been changed.
+     */
     public void setLocation(LatLng location) {
         this.location = location;
     }
 
+    /**
+     * Getter method for the index of this city.
+     * @return Returns the index.
+     * <dt><b>Preconditions:</b></dd> City object must already be instantiated.
+     */
     public int getIndexPos() {
         return indexPos;
     }
 
+    /**
+     * Setter method for the index.
+     * @param indexPos New index.
+     * <dt><b>Preconditions:</b></dd> City object must already be instantiated.
+     * <dt><b>Postconditions:</b></dd> The index has been changed.
+     */
     public void setIndexPos(int indexPos) {
         this.indexPos = indexPos;
     }
 
+    /**
+     * Getter method for city count.
+     * @return Returns the city count.
+     * <dt><b>Preconditions:</b></dd> City object must already be instantiated.
+     * <dt><b>Postconditions:</b></dd> City count has been changed.
+     */
     public static int getCityCount() {
         return cityCount;
     }
 
+    /**
+     * Setter method for the city count.
+     * @param cityCount New city count.
+     * <dt><b>Preconditions:</b></dd> City object must already be instantiated.
+     * <dt><b>Postconditions:</b></dd> City count has been changed.
+     */
     public static void setCityCount(int cityCount) {
         City.cityCount = cityCount;
     }
